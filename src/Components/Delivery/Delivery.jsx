59a81d1fbbe5e9dd2981f2843273a30b9e011e2d
Akshay8281 <aks.pitroda.8@gmail.com>
2025-05-11 17:32:24 +0530

    Extra Images Deleted

--- src/Components/Delivery/Delivery.jsx
+++ src/Components/Delivery/Delivery.jsx
@@ -16,95 +16,71 @@ export default function Delivery() {
   ];
   // Round Card Item 2
 
-  const roundCardItem_2 = [
-    {
-      image: "/public/assets/asset 10.avif",
-      item: "McDonald's",
-      altName: "McDonald's",
-    },
-    {
-      image: "/public/assets/asset 11.avif",
-      item: "La Pino'z Pizza",
-      altName: "La Pino'z Pizza",
-    },
-    {
-      image: "/public/assets/asset 12.avif",
-      item: "Subway",
-      altName: "Subway",
-    },
-    {
-      image: "/public/assets/asset 13.avif",
-      item: "Jay Bhavani Vadapav",
-      altName: "Jay Bhavani Vadapav",
-    },
-    {
-      image: "/public/assets/asset 14.avif",
-      item: "Burger King",
-      altName: "Burger King",
-    },
-    {
-      image: "/public/assets/asset 15.avif",
-      item: "La Milano Pizzeria",
-      altName: "La Milano Pizzeria",
-    },
-  ];
+ const roundCardItem_2 = [
+  { image: "assets/asset 10.avif", item: "McDonald's", altName: "McDonald's" },
+  { image: "assets/asset 11.avif", item: "La Pino'z Pizza", altName: "La Pino'z Pizza" },
+  { image: "assets/asset 12.avif", item: "Subway", altName: "Subway" },
+  { image: "assets/asset 13.avif", item: "Jay Bhavani Vadapav", altName: "Jay Bhavani Vadapav" },
+  { image: "assets/asset 14.avif", item: "Burger King", altName: "Burger King" },
+  { image: "assets/asset 15.avif", item: "La Milano Pizzeria", altName: "La Milano Pizzeria" },
+];
 
-  const boxCardItem = [
-    {
-      boxImage: "/public/assets/asset 18.avif",
-      altName: "PVR Cafe",
-      foodStallName: "PVR Cafe",
-      foodItemInfo: "Fast Food,Momos, Bever...",
-      ratings: "4.0 +",
-      prices: "Rs. 200 for one",
-      duration: "10 min.",
-    },
-    {
-      boxImage: "/public/assets/asset 17.avif",
-      altName: "La Pinos's Pizza",
-      foodStallName: "La Pinos's Pizza",
-      foodItemInfo: "Pizza, Pasta, Italian, Frency....",
-      ratings: "3.0 +",
-      prices: "Rs. 200 for one",
-      duration: "20 min.",
-    },
-    {
-      boxImage: "/public/assets/Bablu Shree Farali Center.avif",
-      altName: "Bablu Shree Farali Center",
-      foodStallName: "Bablu Shree Farali Center",
-      foodItemInfo: "Fast Food, South Indian",
-      ratings: "4.1 +",
-      prices: "Rs. 300 for one",
-      duration: "25 min.",
-    },
-    {
-      boxImage: "/public/assets/Poult.avif",
-      altName: "Poult",
-      foodStallName: "Poult",
-      foodItemInfo: "Fast Food,Burger, Bever...",
-      ratings: "4.0 +",
-      prices: "Rs. 230 for one",
-      duration: "13 min.",
-    },
-    {
-      boxImage: "/public/assets/McDonald's.avif",
-      altName: "Mc Donald's",
-      foodStallName: "Mc Donald's",
-      foodItemInfo: "Burger, Fast Food, Dessert...",
-      ratings: "4.0 +",
-      prices: "Rs. 300 for one",
-      duration: "22 min.",
-    },
-    {
-      boxImage: "/public/assets/Jay Bhavani Vadapav.avif",
-      altName: "Jay Bhavani",
-      foodStallName: "Jay Bhavani",
-      foodItemInfo: "Street Food, Fast Food and More",
-      ratings: "4.2 +",
-      prices: "Rs. 150 for one",
-      duration: "9 min.",
-    },
-  ];
+const boxCardItem = [
+  {
+    boxImage: "assets/asset 18.avif",
+    altName: "PVR Cafe",
+    foodStallName: "PVR Cafe",
+    foodItemInfo: "Fast Food,Momos, Bever...",
+    ratings: "4.0 +",
+    prices: "Rs. 200 for one",
+    duration: "10 min.",
+  },
+  {
+    boxImage: "assets/asset 17.avif",
+    altName: "La Pinos's Pizza",
+    foodStallName: "La Pinos's Pizza",
+    foodItemInfo: "Pizza, Pasta, Italian, Frency....",
+    ratings: "3.0 +",
+    prices: "Rs. 200 for one",
+    duration: "20 min.",
+  },
+  {
+    boxImage: "assets/Bablu Shree Farali Center.avif",
+    altName: "Bablu Shree Farali Center",
+    foodStallName: "Bablu Shree Farali Center",
+    foodItemInfo: "Fast Food, South Indian",
+    ratings: "4.1 +",
+    prices: "Rs. 300 for one",
+    duration: "25 min.",
+  },
+  {
+    boxImage: "assets/Poult.avif",
+    altName: "Poult",
+    foodStallName: "Poult",
+    foodItemInfo: "Fast Food,Burger, Bever...",
+    ratings: "4.0 +",
+    prices: "Rs. 230 for one",
+    duration: "13 min.",
+  },
+  {
+    boxImage: "assets/McDonald's.avif",
+    altName: "Mc Donald's",
+    foodStallName: "Mc Donald's",
+    foodItemInfo: "Burger, Fast Food, Dessert...",
+    ratings: "4.0 +",
+    prices: "Rs. 300 for one",
+    duration: "22 min.",
+  },
+  {
+    boxImage: "assets/Jay Bhavani Vadapav.avif",
+    altName: "Jay Bhavani",
+    foodStallName: "Jay Bhavani",
+    foodItemInfo: "Street Food, Fast Food and More",
+    ratings: "4.2 +",
+    prices: "Rs. 150 for one",
+    duration: "9 min.",
+  },
+];
   return (
     <>
       {/* <Nav-Buttons */}
